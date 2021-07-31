6dc4b89bb5b451695ee9115daef0712437e20766
itt-icc <66052121+itt-icc@users.noreply.github.com>
2021-07-31 23:56:58 +0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,65 @@
+#ifndef _GLIBCXX_NO_ASSERT
+
+
+#include <cassert>
+
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+
+/*这里把常用的库文件集中在这里编译*/
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+//#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <map>
+
+
+#define PLAYERCOUNT     3           //玩家数量
+#define CARDSCOUNT      54          //牌数量
+#define CURRENTPLAYER   0           //当前玩家下标
+#define VALUECOUNT      17          //玩家基本手牌数量
+#define ERROR           -1          //错误指示
+
+#endif
